5d95c4f21b8e2afc38b0dd4da3769b54b07d9d86
Thanathip Kumnarai <feaw.book@gmail.com>
2023-04-01 22:29:23 +0700

    init project

---
+++ books-social/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  books-social
+//
+//  Created by ธนาธิป ก๋ำนารายณ์ on 1/4/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ books-social/books_socialApp.swift
@@ -0,0 +1,17 @@
+//
+//  books_socialApp.swift
+//  books-social
+//
+//  Created by ธนาธิป ก๋ำนารายณ์ on 1/4/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct books_socialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
